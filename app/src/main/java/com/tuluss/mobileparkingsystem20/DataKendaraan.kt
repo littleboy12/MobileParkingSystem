d14d1651218fd22d266767e1b6978b3da71f7a21
IyaTulus <75067698+littleboy12@users.noreply.github.com>
2024-12-30 12:30:57 +0700

    Melengkapi Kekurangan

---
+++ app/src/main/java/com/tuluss/mobileparkingsystem20/DataKendaraan.kt
@@ -0,0 +1,4 @@
+package com.tuluss.mobileparkingsystem20
+
+class DataKendaraan {
+}
---
+++ app/src/main/java/com/tuluss/mobileparkingsystem20/KendaraanActivity.kt
@@ -0,0 +1,11 @@
+package com.tuluss.mobileparkingsystem20
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class KendaraanActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kendaraan)
+    }
+}
